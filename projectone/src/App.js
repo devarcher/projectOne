7fd0893cdf877f3565bda1d8fb3f459d15ee360f
devarcher <archershot@hotmail.com>
2020-01-07 11:28:52 -0600

    added list item functionality. Added focus on input field.

--- projectone/src/App.js
+++ projectone/src/App.js
@@ -4,28 +4,50 @@ import { render } from '@testing-library/react';
 
 
 class todoList extends Component {
+  textInput = React.createRef()
+
   state = {
   text: '',
   todos: [],
-  isClicked: false
+  isClicked: false,
   }
 
-  wasClicked = () => {
-    this.setState({ isClicked: !this.state.isClicked})
-    console.log(this.state.isClicked)
+  componentDidMount() {
+    this.textInput.current.focus();
   }
 
-  textInput = (e) => {
+  todoButtonHandler = () => {
+    // Save below 2 lines of code for another project
+    // this.setState({ isClicked: !this.state.isClicked })
+    // console.log(this.state.isClicked)
+    
+    // Move submitted text to todos array
+    this.state.todos.push(this.state.text);
+    // console.log('todoButtonHandler() todo Push: ', this.state.todos);
+
+    // Clear input field
+    this.setState({ text: ''});
+
+    // Set Focus on Text Input Field
+    this.componentDidMount();
+  }
+
+  inputFieldText = (e) => {
     this.setState( {text: e.target.value })
+    // console.log(e.target.value);
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.text}</h1>
-        <button onClick={this.wasClicked}>Click Me</button>
-        <input value={this.state.text} onChange={this.textInput}></input>
-
+        <button onClick={this.todoButtonHandler}>Click Me</button>
+        <input ref={this.textInput} value={this.state.text} onChange={this.inputFieldText}></input>
+        <ul>
+          {this.state.todos.map((todo, index) => 
+            <li key={index}>{todo}</li>
+          )}
+        </ul>
       </div>
     )
   }
