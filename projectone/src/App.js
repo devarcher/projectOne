a4af87339cf176123ea2b5d611bf5858ddd5a2e7
devarcher <archershot@hotmail.com>
2020-01-07 11:53:41 -0600

    fix bug in input field focus

--- projectone/src/App.js
+++ projectone/src/App.js
@@ -4,26 +4,30 @@ import { render } from '@testing-library/react';
 
 
 class todoList extends Component {
+  // Create a reference to grab input field
   textInput = React.createRef()
 
+  // Create State object YAY, my prayers have been answered.
   state = {
-  text: '',
-  todos: [],
-  isClicked: false,
+    text: '',
+    todos: [],
+    isClicked: false,
   }
 
+  // Function to set focus on text input Field
   componentDidMount() {
     this.textInput.current.focus();
   }
 
-  todoButtonHandler = () => {
+  // Actions taken when Add Todo button is clicked.
+  addTodoButtonHandler = () => {
     // Save below 2 lines of code for another project
     // this.setState({ isClicked: !this.state.isClicked })
     // console.log(this.state.isClicked)
     
     // Move submitted text to todos array
     this.state.todos.push(this.state.text);
-    // console.log('todoButtonHandler() todo Push: ', this.state.todos);
+    // console.log('addTodoButtonHandler() todo Push: ', this.state.todos);
 
     // Clear input field
     this.setState({ text: ''});
@@ -41,7 +45,7 @@ class todoList extends Component {
     return (
       <div>
         <h1>{this.state.text}</h1>
-        <button onClick={this.todoButtonHandler}>Click Me</button>
+        <button onClick={this.addTodoButtonHandler}>Add Item</button>
         <input ref={this.textInput} value={this.state.text} onChange={this.inputFieldText}></input>
         <ul>
           {this.state.todos.map((todo, index) => 
