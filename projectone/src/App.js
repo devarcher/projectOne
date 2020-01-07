db4be7c783d620e32e2aeec7cf4e5f961c8456aa
devarcher <archershot@hotmail.com>
2020-01-06 20:53:31 -0600

    added functional switch on button

--- projectone/src/App.js
+++ projectone/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class Clickey extends Component {
+  state = {
+  text: '',
+  todos: [],
+  isClicked: false
+  }
+
+  wasClicked = () => {
+    this.setState({ isClicked: !this.state.isClicked})
+    console.log(this.state.isClicked)
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.wasClicked}>Click Me</button>
+      </div>
+    )
+  }
+};
+
+export default Clickey;
